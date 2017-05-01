d98c2b592e043f54aa037274e90f2f10fc0e0ffd
karthik <karthkk@gmail.com>
2017-04-30 22:48:03 -0700

    aitp train model

--- network_models/multicam_net.py
+++ network_models/multicam_net.py
@@ -11,30 +11,24 @@ class PurchasePredNet(Network):
         self.inputs = []
         self.left_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
         self.right_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
-        self.layers = dict({'left':self.left_img, 'right':self.right_img})
+        self.motor_state = tf.placeholder(tf.float32, shape=(None, 6))
+        self.layers = dict({'left':self.left_img, 'right':self.right_img, 'motor_state': self.motor_state})
+
         self.trainable = trainable
         self.setup()
 
     def setup(self):
         (self.feed('left')
-             .conv(3, 3, 64, 1, 1, name='l_conv1_1', trainable=False)
-             .conv(3, 3, 64, 1, 1, name='l_conv1_2', trainable=False)
+             .conv(3, 3, 64, 2, 2, name='l_conv1_1', trainable=False)
+             .conv(3, 3, 64, 2, 2, nam    e='l_conv1_2', trainable=False)
              .max_pool(2, 2, 2, 2, padding='VALID', name='l_pool1')
              .conv(3, 3, 128, 1, 1, name='l_conv2_1', trainable=False)
              .conv(3, 3, 128, 1, 1, name='l_conv2_2', trainable=False)
-             .max_pool(2, 2, 2, 2, padding='VALID', name='l_pool2')
-             .conv(3, 3, 256, 1, 1, name='l_conv3_1')
-             .conv(3, 3, 256, 1, 1, name='l_conv3_2')
-             .conv(3, 3, 256, 1, 1, name='l_conv3_3')
-             .max_pool(2, 2, 2, 2, padding='VALID', name='l_pool3')
-             .conv(3, 3, 512, 1, 1, name='l_conv4_1')
-             .conv(3, 3, 512, 1, 1, name='l_conv4_2')
-             .conv(3, 3, 512, 1, 1, name='l_conv4_3')
-             .max_pool(2, 2, 2, 2, padding='VALID', name='l_pool4')
              .conv(3, 3, 128, 1, 1, name='l_conv5_1_a')
-             .conv(3, 3, 128, 1, 1, name='l_conv5_2_a')
-             .conv(3, 3, 128, 1, 1, name='l_conv5_3_a')
-             .fc(4096, name='l_fc6_a'))
+             .conv(3, 3, 64, 1, 1, name='l_conv5_2_a')
+             .conv(3, 3, 48, 1, 1, name='l_conv5_3_a')
+             .spatial_softmax(name='l_ss'))
+
 
         (self.feed('right')
              .conv(3, 3, 64, 1, 1, name='r_conv1_1', trainable=False)
@@ -42,21 +36,14 @@ class PurchasePredNet(Network):
              .max_pool(2, 2, 2, 2, padding='VALID', name='r_pool1')
              .conv(3, 3, 128, 1, 1, name='r_conv2_1', trainable=False)
              .conv(3, 3, 128, 1, 1, name='r_conv2_2', trainable=False)
-             .max_pool(2, 2, 2, 2, padding='VALID', name='r_pool2')
-             .conv(3, 3, 256, 1, 1, name='r_conv3_1')
-             .conv(3, 3, 256, 1, 1, name='r_conv3_2')
-             .conv(3, 3, 256, 1, 1, name='r_conv3_3')
-             .max_pool(2, 2, 2, 2, padding='VALID', name='r_pool3')
-             .conv(3, 3, 512, 1, 1, name='r_conv4_1')
-             .conv(3, 3, 512, 1, 1, name='r_conv4_2')
-             .conv(3, 3, 512, 1, 1, name='r_conv4_3')
-             .max_pool(2, 2, 2, 2, padding='VALID', name='r_pool4')
              .conv(3, 3, 128, 1, 1, name='r_conv5_1_a')
-             .conv(3, 3, 128, 1, 1, name='r_conv5_2_a')
-             .conv(3, 3, 128, 1, 1, name='r_conv5_3_a')
-             .fc(1024, name='r_fc6_a') )
-        (self.feed('l_fc6_a', 'r_fc6_a').concat(axis=1, name='merged')
-             .fc(256, name='fc7_a')
+             .conv(3, 3, 64, 1, 1, name='r_conv5_2_a')
+             .conv(3, 3, 48, 1, 1, name='r_conv5_3_a')
+             .spatial_softmax(name='r_ss'))
+
+        (self.feed('l_ss', 'r_ss', 'motor_state').concat(axis=1, name='merged')
+             .fc(48, name='fc7_a')
+             .fc(24, name='fc8_a')
              .fc(12, relu=False, name='out'))
 
     def vgg_to_transforms(sellf, datapath, transforms):
@@ -81,7 +68,8 @@ class PurchasePredNet(Network):
 def main():
     nw = PurchasePredNet(True)
     sess = tf.Session()
-    nw.load('/tmp/VGG_imagenet.npy', sess, True)
+    # nw.load('/tmp/VGG_imagenet.npy', sess, True)
+    nw.load('/data/image/models/VGG_imagenet.npy', sess, True)
     print('Network loaded successfully')
 
 if __name__ == '__main__':
