ced0a606b60fe02b4a5de8f2f655750ac0176970
Karthik <karthkk@gmail.com>
2017-05-03 09:56:42 -0700

    Trainer that abstracts all training of the robot model

--- network_models/multicam_net.py
+++ network_models/multicam_net.py
@@ -1,52 +1,16 @@
-import tensorflow as tf
+import cv2
 import numpy as np
-
+import tensorflow as tf
 from tftools.network import Network
 
 n_classes = 12
 
-class PurchasePredNet(Network):
-
-    def __init__(self, trainable=True):
-        self.inputs = []
-        self.left_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
-        self.right_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
-        self.motor_state = tf.placeholder(tf.float32, shape=(None, 6))
-        self.layers = dict({'left':self.left_img, 'right':self.right_img, 'motor_state': self.motor_state})
-
-        self.trainable = trainable
-        self.setup()
-
-    def setup(self):
-        (self.feed('left')
-             .conv(3, 3, 64, 2, 2, name='l_conv1_1', trainable=False)
-             .conv(3, 3, 64, 2, 2, nam    e='l_conv1_2', trainable=False)
-             .max_pool(2, 2, 2, 2, padding='VALID', name='l_pool1')
-             .conv(3, 3, 128, 1, 1, name='l_conv2_1', trainable=False)
-             .conv(3, 3, 128, 1, 1, name='l_conv2_2', trainable=False)
-             .conv(3, 3, 128, 1, 1, name='l_conv5_1_a')
-             .conv(3, 3, 64, 1, 1, name='l_conv5_2_a')
-             .conv(3, 3, 48, 1, 1, name='l_conv5_3_a')
-             .spatial_softmax(name='l_ss'))
+def resize_to_model(im, shape=(224, 224)):
+    return cv2.resize(im, shape)
 
 
-        (self.feed('right')
-             .conv(3, 3, 64, 1, 1, name='r_conv1_1', trainable=False)
-             .conv(3, 3, 64, 1, 1, name='r_conv1_2', trainable=False)
-             .max_pool(2, 2, 2, 2, padding='VALID', name='r_pool1')
-             .conv(3, 3, 128, 1, 1, name='r_conv2_1', trainable=False)
-             .conv(3, 3, 128, 1, 1, name='r_conv2_2', trainable=False)
-             .conv(3, 3, 128, 1, 1, name='r_conv5_1_a')
-             .conv(3, 3, 64, 1, 1, name='r_conv5_2_a')
-             .conv(3, 3, 48, 1, 1, name='r_conv5_3_a')
-             .spatial_softmax(name='r_ss'))
-
-        (self.feed('l_ss', 'r_ss', 'motor_state').concat(axis=1, name='merged')
-             .fc(48, name='fc7_a')
-             .fc(24, name='fc8_a')
-             .fc(12, relu=False, name='out'))
-
-    def vgg_to_transforms(sellf, datapath, transforms):
+class MulticamRobotNet(Network):
+    def vgg_to_transforms(self, datapath, transforms):
         dat = np.load(datapath)
         model = dat.item()
         out = {}
@@ -63,6 +27,99 @@ class PurchasePredNet(Network):
         f_right = lambda x: 'r_' + x if x.startswith('conv') else x
         out_dict = self.vgg_to_transforms(data_path, [f_left, f_right])
         self.load_from_dict(out_dict, ignore_missing, session)
+
+
+class PurchasePredNetHome(MulticamRobotNet):
+
+    def __init__(self, trainable=True):
+        self.inputs = []
+        self.left_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+        self.right_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+        self.motor_state = tf.placeholder(tf.float32, shape=(None, 6))
+        self.layers = dict({'left':self.left_img, 'right':self.right_img, 'motor_state': self.motor_state})
+
+        self.trainable = trainable
+        self.setup()
+
+    def setup(self):
+        (self.feed('left')
+             .conv(7, 7, 64, 2, 2, name='l_conv1_7x7_s2', trainable=False)
+             .conv(5, 5, 64, 1, 1, name='l_conv2')
+             .conv(5, 5, 64, 1, 1, name='l_conv3')
+             .spatial_softmax(name='l_ss'))
+
+
+        (self.feed('right')
+             .conv(7, 7, 64, 2, 2, name='r_conv1_7x7_s2', trainable=False)
+             .conv(5, 5, 64, 1, 1, name='r_conv2')
+             .conv(5, 5, 64, 1, 1, name='r_conv3')
+             .spatial_softmax(name='r_ss'))
+
+        (self.feed('l_ss', 'r_ss', 'motor_state').concat(axis=1, name='merged')
+             .fc(96, name='fc6_a')
+             .fc(24, name='fc8_a')
+             .fc(12, relu=False, name='out'))
+
+    def predict(self, sess, left_im, right_im, motor_state):
+        out_pred = sess.run(tf.nn.softmax(self.layers['out']),
+                            {self.left_img: resize_to_model(left_im).reshape((1, 224, 224, 3)),
+                             self.right_img: resize_to_model(right_im).reshape((1, 224, 224, 3)),
+                             self.motor_state: motor_state})
+        choice = np.argmax(out_pred)
+        motor = choice / 2
+        direction = choice % 2 * 2 - 1
+        print(motor, direction)
+        return (motor, direction)
+
+
+class PurchasePredNetOffice(MulticamRobotNet):
+
+    def __init__(self, trainable=True):
+        self.inputs = []
+        self.main_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+        self.front_img = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+        self.motor_state = tf.placeholder(tf.float32, shape=(None, 6))
+        self.depth = tf.placeholder(tf.float32, shape=[None, 112*112])
+        self.layers = dict({'main':self.main_img, 'front':self.front_img,
+                            'motor_state': self.motor_state, 'depth': self.depth})
+
+        self.trainable = trainable
+        self.setup()
+
+    def setup(self):
+        (self.feed('main')
+             .conv(7, 7, 64, 2, 2, name='l_conv1_7x7_s2', trainable=False)
+             .conv(5, 5, 64, 1, 1, name='l_conv2')
+             .conv(5, 5, 64, 1, 1, name='l_conv3')
+             .spatial_softmax(name='l_ss'))
+
+
+        (self.feed('front')
+             .conv(7, 7, 64, 2, 2, name='r_conv1_7x7_s2', trainable=False)
+             .conv(5, 5, 64, 1, 1, name='r_conv2')
+             .conv(5, 5, 64, 1, 1, name='r_conv3')
+             .spatial_softmax(name='r_ss'))
+
+        (self.feed('l_conv3', 'depth')
+             .depth_lookup(name='filter_depth'))
+
+        (self.feed('l_ss', 'r_ss', 'filter_depth', 'motor_state').concat(axis=1, name='merged')
+             .fc(108, name='fc6_a')
+             .fc(36, name='fc8_a')
+             .fc(12, relu=False, name='out'))
+
+
+
+    def predict(self, sess, main_im, front_im, depth, motor_state):
+        out_pred = sess.run(tf.nn.softmax(self.layers['out']),
+                            {self.main: resize_to_model(main_im).reshape((1, 224, 224, 3)),
+                             self.front: resize_to_model(front_im).reshape((1, 224, 224, 3)),
+                             self.depth: resize_to_model(depth, (112,112)).reshape((1, 112*112)),
+                             self.motor_state: motor_state})
+        choice = np.argmax(out_pred)
+        motor = choice / 2
+        direction = choice % 2 * 2 - 1
+        return (motor, direction)
 
 
 def main():
--- network_models/trainer.py
+++ network_models/trainer.py
@@ -1,33 +1,173 @@
 import tensorflow as tf
+from tftools import data
+import sys
+import os
+import multicam_net
+import numpy as np
+import urllib2
+from operator import mul
+import cv2
+
+google_net_location = '/data/image/models/googlenet.npy'
+
+def motor_2_label(motor, direction):
+    return 2*motor+(direction+1)/2
+
+def from_buffer(im_str, sz, dtype_dat):
+    f1 = np.fromstring(im_str, dtype=dtype_dat)
+    return f1.reshape(sz)
+
+
+class Trainer():
+
+    BATCH_SIZE = 8
+
+    def __init__(self, sess, model_location, train_data_location, episode_num):
+        self.model_location = model_location
+        self.train_data_location =train_data_location
+        self.episode = episode_num
+        self.sess = sess
+        self.saver = None
+        self.setup_models()
+        self.setup_trainer()
+
+
+    def setup_trainer(self):
+        self.labels_pl = tf.placeholder(shape=(None,), dtype=tf.int32)
+        self.loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.net.layers['out'], labels=self.labels_pl))
+        self.optimizer = tf.train.AdagradOptimizer(0.001)
+        self.train = self.optimizer.minimize(self.loss)
+        self.sess.run(tf.global_variables_initializer())
+        model_files_available = os.listdir(self.model_location)
+        if not model_files_available:
+            self.net.load(google_net_location)
+        else:
+            if self.saver is None:
+                self.saver = tf.train.Saver()
+            self.saver.restore(self.sess, self.model_save_path())
+
+    def retrain_model(self, current_episode, steps=100, print_every=10):
+        training_data, total_samples = self.load_all_training_data()
+        self.episode = current_episode
+        for step in range(steps):
+            idxes = np.random.randint(0, total_samples - 1, size=Trainer.BATCH_SIZE)
+            batch_data = self.get_batch(training_data, idxes)
+            [_, lv] = self.sess.run([self.train, self.loss], batch_data)
+            if step%print_every == 0:
+                print("Episode: %d, Step: %s, Loss: %f"%(current_episode, step, lv))
+        self.saver.save(self.sess, self.model_save_path())
+
+    def model_save_path(self):
+        return self.model_location + '/episode_model_%d' % self.episode
+
+
+class OfficeTrainer(Trainer):
+
+
+    col_sz = (480, 640, 3)
+    dep_sz = (480, 640)
+    pnt_sz = (480, 640, 3)
+    cad_sz = (480, 640, 3)
+    fcam_sz = (288, 352, 3)
+
+    col_dtype = np.uint8
+    dep_dtype = np.uint16
+    cad_dtype = np.uint8
+    pnt_dtype = np.float32
+
+    ROBOT_URL = 'http://robot.local:8888/robot/%d/%d/%d/%d/%d/%d/'
+    FRONT_CAMERA_URL = 'http://localhost:8891/camera'
+    MAIN_CAMERA_URL = 'http://robot.local:8888/camera'
+
+
+    def get_front_camera(self):
+        response = urllib2.urlopen(OfficeTrainer.FRONT_CAMERA_URL)
+        fc = from_buffer(response.read(288 * 352 * 3), OfficeTrainer.fcam_sz, np.uint8)
+        response.close()
+        return fc
+
+    def get_images(self):
+        response = urllib2.urlopen(OfficeTrainer.MAIN_CAMERA_URL)
+        col = from_buffer(response.read(reduce(mul, OfficeTrainer.col_sz)), OfficeTrainer.col_sz, OfficeTrainer.col_dtype)
+        pnt = from_buffer(response.read(reduce(mul, OfficeTrainer.pnt_sz) * 4), OfficeTrainer.pnt_sz, OfficeTrainer.pnt_dtype)
+        cad = from_buffer(response.read(reduce(mul, OfficeTrainer.cad_sz)), OfficeTrainer.cad_sz, OfficeTrainer.cad_dtype)
+        dep = from_buffer(response.read(reduce(mul, OfficeTrainer.dep_sz) * 2), OfficeTrainer.dep_sz, OfficeTrainer.dep_dtype)
+        response.close()
+        return col, pnt, cad, dep, self.get_front_camera()
+
+    def predict(self, sess, motor_state):
+        col, pnt, cad, dep, fnt = self.get_images()
+        return self.net.predict(sess, cad, fnt, dep, motor_state)
+
+    def get_data_to_save(self):
+        col, pnt, cad, dep, fnt = self.get_images()
+        return [["main", cad, data.IMG_TYP],
+        ["front", fnt, data.IMG_TYP],
+        ['depth', dep, data.NPY_TYP]]
+
+    def load_all_training_data(self):
+        train_files = [os.path.join(self.train_data_location, f) for f in os.listdir(self.train_data_location)
+                       if f.endswith('.tfrecords')]
+        img_cnt = 0
+        for train_fl in train_files:
+            img_cnt += data.TFDataReader(train_fl).count()
+        train_inp = np.zeros([2, img_cnt, 224, 224, 3], dtype=np.uint8)
+        train_labels = np.zeros([img_cnt, ], dtype=np.int32)
+        train_states = np.zeros([img_cnt, 6], dtype=np.float32)
+        train_depths = np.zeros([img_cnt, 112 * 112], dtype=np.float32)
+        idx = 0
+        for train_fl in train_files:
+            reader = data.TFDataReader(train_fl)
+            fds = reader.readall((["main", data.IMG_TYP],
+                                  ["front", data.IMG_TYP],
+                                  ["depth", data.NPY_TYP],
+                                  ['motor', data.INT_TYP],
+                                  ['direction', data.INT_TYP],
+                                  ['motor_state', data.FLT_TYP]))
+
+            for fd in fds:
+                train_inp[0, idx, ...] = multicam_net.resize_to_model(fd['main'])
+                train_inp[1, idx, ...] = multicam_net.resize_to_model(fd['front'])
+                train_depths[idx, ...] = multicam_net.resize_to_model(fd['depth'], (112, 112)).reshape((112 * 112))
+                motor = int(fd['motor'][0])
+                direction = int(fd['direction'][0])
+                train_states[idx, :] = np.array(fd['motor_state'])
+                label = motor_2_label(motor, direction)
+                train_labels[idx] = label
+                idx += 1
+        return ((train_inp, train_labels, train_states, train_depths), img_cnt)
+
+    def get_batch(self, training_data, idxes):
+        (train_inp, train_labels, train_states, train_depths) = training_data
+        main_im = train_inp[0, idxes, ...]
+        front_im = train_inp[1, idxes, ...]
+        labels_x = train_labels[idxes]
+        depth_x = train_depths[idxes, ...]
+        states = train_states[idxes, ...]
+        return {self.labels_pl: labels_x,
+                self.net.main_img: main_im,
+                self.net.front_img: front_im,
+                self.net.depth: depth_x,
+                self.net.motor_state: states}
+
+    def setup_models(self):
+        self.net = multicam_net.PurchasePredNetOffice()
+
+class HomeTrainer(Trainer):
+
+    def setup_models(self):
+        self.net = multicam_net.PurchasePredNetHome()
+
+    pass
 
 
 
 
-def read_and_decode(filename_queue):
-    reader = tf.TFRecordReader()
-    _, serialized_example = reader.read(filename_queue)
-    features = tf.parse_single_example(
-      serialized_example,
-      # Defaults are not specified since both keys are required.
-      features={
-          'motor': tf.FixedLenFeature([], tf.int64),
-          'direction': tf.FixedLenFeature([], tf.int64),
-          'state': tf.FixedLenFeature([], tf.string ),
-          'left_image': tf.VarLenFeature([], tf.string),
-          'right_image': tf.FixedLenFeature([], tf.string),
-      })
+#                     writer.add( [["left_image", left_im, data.IMG_TYP],
+#                                 ["right_image", right_im, data.IMG_TYP],
+#                                 ['motor', [motor], data.INT_TYP],
+#                                 ['direction', [direction], data.INT_TYP],
+#                                 ['motor_state', [motor_state[i] for i in range(6)], data.INT_TYP]])
 
-    # Convert from a scalar string tensor (whose single string has
-    # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
-    # [mnist.IMAGE_PIXELS].
-    left_image = extract_image(features, 'left_image')
-    right_image = extract_image(features, 'right_image')
-x    label = tf.cast(features['label'], tf.int32)
-    return image,
-
-
-def extract_image(features, feature_name):
-    image_flat = tf.decode_raw(features[feature_name], tf.uint8)
-    image = tf.image.resize_images(tf.reshape(image_flat, [480, 640, 3]), [224, 224, 3])
-    return tf.cast(image, tf.float16) * (1. / 255) - 0.5
 
